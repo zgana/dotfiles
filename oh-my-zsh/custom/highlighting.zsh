d4758de2b30035cd9e77f70e1cd5f9c71505ee42
Michael Richman <mike.d.richman@gmail.com>
2024-02-29 13:10:31 -0500

    Fix syntax highlighting theme

---
+++ oh-my-zsh/custom/highlighting.zsh
@@ -0,0 +1,14 @@
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+ZSH_HIGHLIGHT_STYLES[alias]=fg=white,bold
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=white,bold
+ZSH_HIGHLIGHT_STYLES[function]=fg=white,bold
+ZSH_HIGHLIGHT_STYLES[command]=fg=white,bold
+ZSH_HIGHLIGHT_STYLES[precommand]=none
+ZSH_HIGHLIGHT_STYLES[hashed-command]=none
+ZSH_HIGHLIGHT_STYLES[path]=none
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=cyan,bold
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=blue,bold
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=fg=cyan
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=cyan,bold
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=blue,bold
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=cyan
