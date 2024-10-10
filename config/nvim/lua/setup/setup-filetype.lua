f9ee4059abab94f54b92906877ba61b918b5793b
Michael Richman <mike.d.richman@gmail.com>
2024-10-10 13:52:49 -0400

    vim: filetype tweaks

--- config/nvim/lua/setup/setup-filetype.lua
+++ config/nvim/lua/setup/setup-filetype.lua
@@ -1,12 +1,12 @@
 vim.filetype.add({
   pattern = {
     -- jinja templates
-    [".*/.*py.*jinja"] = "python",
-    [".*/.*toml.*jinja"] = "toml",
-    [".*/.*.ya?ml.*jinja"] = "yaml",
+    [".*/.*%.py.*%.jinja"] = "python",
+    [".*/.*%.toml.*%.jinja"] = "toml",
+    [".*/.*%.ya?ml.*%.jinja"] = "yaml",
 
     -- e.g. cloudformation
-    [".*/*.template"] = "yaml",
+    [".*/*%.template"] = "yaml",
   },
 })
 
