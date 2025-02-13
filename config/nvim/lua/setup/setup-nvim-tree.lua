fbaaf8718ce0c74a3c974759c00c7446404408c2
Michael Richman <mike.d.richman@gmail.com>
2025-02-12 23:34:23 -0500

    nvim: drop redundant netrw config

--- config/nvim/lua/setup/setup-nvim-tree.lua
+++ config/nvim/lua/setup/setup-nvim-tree.lua
@@ -1,10 +1,3 @@
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- OR setup with some options
 require("nvim-tree").setup({
   sort = {
