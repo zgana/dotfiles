62bc8d7cb78d1bf24f41788b9ef04ae2f6f5306f
Michael Richman <mike.d.richman@gmail.com>
2025-03-31 11:19:09 -0400

    nvim + tmux: theme and color corrections

--- config/nvim/lua/setup/setup-colorscheme.lua
+++ config/nvim/lua/setup/setup-colorscheme.lua
@@ -1,8 +1,20 @@
 local chosen = 'kanagawa'
 
--- apply any needed configuration
-vim.g.everforest_background = 'hard'
-vim.g.everforest_disable_italic_comment = true
+-- apply kanagawa configuration
+require("kanagawa").setup({
+  commentStyle = { italic = false },
+  keywordStyle = { italic = false },
+  overrides = function()
+    return {
+      ["@variable.builtin"] = { italic = false },
+    }
+  end
+})
+
+
+-- -- apply everforest configuration
+-- vim.g.everforest_background = 'hard'
+-- vim.g.everforest_disable_italic_comment = true
 
 -- finally apply the colorscheme
 vim.cmd.colorscheme(chosen)
