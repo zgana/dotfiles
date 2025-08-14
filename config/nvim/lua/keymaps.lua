972cc50933076195a809c2b2d33d1d6f77bc1b74
Michael Richman <michael.richman@lgads.tv>
2025-08-14 15:01:09 -0400

    nvim: update diagnostic maps

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -28,6 +28,8 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Diagnostic keymaps
 vim.keymap.set('n', '[0', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']0', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 
 -- commandline backscroll
